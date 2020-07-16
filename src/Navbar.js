3662a9f7481dc66a42aba35499e5c2d0e3e8f8f9
xtel <evstafyevan96@gmail.com>
2020-07-16 19:19:26 +0500

    Добавил компонент Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {View, StyleSheet, Text} from 'react-native'
+
+export const Navbar = ({title}) => {
+    return (
+        <View style={styles.navbar}>
+            <Text style={styles.text}> {title} </Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    navbar: {
+        height: 70,
+        backgroundColor: 'blue',
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    }, 
+    text: {
+        fontSize: 24,
+        color: '#fff'
+    }
+})
